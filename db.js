4beb8d6ba90d6abd3ecf09bdebcfb7d5106fdd02
IzYoBoiJay <JTCalahatian@gmail.com>
2021-11-20 17:51:16 -0500

    Database Client Integration with Test

---
+++ db.js
@@ -0,0 +1,21 @@
+const {Client} = require('pg')
+
+const client = new Client({
+    host: "localhost",
+    user: "postgres",
+    port: 5432,
+    password: "password",
+    database: "GroupProject"
+})
+
+client.connect();
+
+client.query(`SELECT * FROM Customer ORDER BY username ASC`, (err, res)=>{
+    if(!err){
+        console.log(res.rows);
+    }
+    else{
+        console.log(err.message);
+    }
+    client.end;
+})
